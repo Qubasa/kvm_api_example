c5bd59f13df3ca85e3fd669e6b8b976871603a16
Luis Hebendanz <luis.nixos@gmail.com>
2020-09-17 10:45:03 +0200

    Better stacktraces

--- shell.nix
+++ shell.nix
@@ -3,8 +3,12 @@
     buildInputs = with pkgs; [
       clang_10
       llvmPackages_10.lld
+      llvmPackages_10.bintools
       cmake
       gnumake
     ];
-    shellHook = "export HISTFILE=${toString ./.history}";
+    shellHook = ''
+      export HISTFILE=${toString ./.history}
+      export UBSAN_OPTIONS=print_stacktrace=1
+      '';
   }
