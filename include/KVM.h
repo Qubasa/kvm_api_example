2c6f9a08313eb9714260dbc4248f9294fc87f250
Luis Hebendanz <luis.nixos@gmail.com>
2020-09-19 02:21:09 +0200

    Real mode add ax,42 does not work :(

--- include/KVM.h
+++ include/KVM.h
@@ -21,9 +21,22 @@
 using std::cout, std::cerr, std::endl, fmt::format;
 using std::showbase, std::hex;
 
-const unsigned int PAGE_SIZE = 0x1000;
+const unsigned int PAGE_SIZE = getpagesize();
 
-template <typename... Args> void panic(std::string error, Args... args) {
+#ifdef MY_DEBUG
+constexpr bool DEBUG = true;
+#else
+constexpr bool DEBUG = false;
+#endif
+
+template <typename... Args>
+constexpr void dbg(std::string_view msg, Args... args) {
+  if constexpr (DEBUG) {
+    fmt::print(format("DEBUG: {}", msg), args...);
+  }
+}
+
+template <typename... Args> void panic(std::string_view error, Args... args) {
   cerr << "ERROR: " << strerror(errno) << endl;
 
   throw std::runtime_error(format(error, args...));
@@ -36,17 +49,17 @@ private:
 public:
   size_t max_vcpus;
   size_t max_vcpu_mmap_size;
+  size_t max_nr_mem_slots;
   KVM() {
     kvm_fd = open("/dev/kvm", O_RDWR);
     if (kvm_fd < 0) {
       panic("failed to open /dev/kvm");
     }
 
-    cout << "KVM fd: " << kvm_fd << endl;
-
     max_vcpus = cap(KVM_CAP_NR_VCPUS);
     max_vcpu_mmap_size =
         send(KVM_GET_VCPU_MMAP_SIZE, 0); // Why do I need a 0 as arg?
+    max_nr_mem_slots = cap(KVM_CAP_NR_MEMSLOTS);
   }
 
   template <typename... Args> int inline send(int request, Args... args) {
@@ -84,18 +97,18 @@ private:
   int id;
   size_t max_vcpus;
   size_t max_vcpu_mmap_size;
-  std::vector<struct vcpu> vcpus_vec;
+  size_t max_nr_mem_slots;
 
 public:
-  std::vector<struct kvm_userspace_memory_region> memory_vec;
-  struct kvm_userspace_memory_region mem;
+  std::vector<struct vcpu> vcpus_vec;
+  std::vector<struct kvm_userspace_memory_region> mem_vec;
 
   VM(KVM &kvm, int id) : id(id) {
     vm_fd = kvm.send(KVM_CREATE_VM, id);
-    cout << "Created VM with id " << id << " and fd: " << vm_fd << endl;
 
     max_vcpu_mmap_size = kvm.max_vcpu_mmap_size;
     max_vcpus = kvm.max_vcpus;
+    max_nr_mem_slots = kvm.max_nr_mem_slots;
   }
 
   template <typename... Args> int inline send(int request, Args... args) {
@@ -106,13 +119,21 @@ public:
     return ret;
   }
 
-  void add_mem_pages(size_t pages) {
+  void add_mem_pages(size_t pages, unsigned int slot, unsigned int flags = 0) {
     assert(pages > 0);
+
+    if (slot > max_nr_mem_slots) {
+      panic("max mem slots reached");
+    }
+
+    for (const auto &m : mem_vec) {
+      if (m.slot == slot) {
+        panic("mem_page with slot id {} already exists", slot);
+      }
+    }
 
     // Calc number of pages
     size_t mem_size = PAGE_SIZE * pages;
-    cout << "Mapping " << hex << showbase << mem_size << " bytes of vm memory"
-         << endl;
 
     // map memory
     char *mem_ptr = reinterpret_cast<char *>(
@@ -127,17 +148,17 @@ public:
 
     // Setup vm memory
     struct kvm_userspace_memory_region mem_arg;
-    mem_arg.slot = 0;
-    mem_arg.flags = 0;
+    mem_arg.slot = slot;
+    mem_arg.flags = flags;
     mem_arg.memory_size = mem_size;
     mem_arg.userspace_addr = (unsigned long)mem_ptr;
 
-    // Calc last guest phys address
-    if (!memory_vec.empty()) {
-      const auto &last_mem = memory_vec.back();
-      mem_arg.guest_phys_addr = last_mem.guest_phys_addr + last_mem.memory_size;
-    } else {
+    // Calculate memory phys addr
+    if (mem_vec.empty()) {
       mem_arg.guest_phys_addr = 0;
+    } else {
+      const auto &old_mem = mem_vec.back();
+      mem_arg.guest_phys_addr = old_mem.guest_phys_addr + old_mem.memory_size;
     }
 
     // Set memory
@@ -148,8 +169,7 @@ public:
       throw;
     }
 
-    // Save memory blob
-    memory_vec.emplace_back(mem_arg);
+    mem_vec.emplace_back(std::move(mem_arg));
   }
 
   void add_vcpu() {
@@ -196,13 +216,49 @@ public:
 
     memset(&regs, 0, sizeof(regs));
 
-    /* regs.rflags = 2; */
-    regs.rip = 0; // Instruction pointer
+    regs.rflags = 0x2; // VT-X needs this
+    regs.rip = 0;      // Instruction pointer
 
     send_vcpu(KVM_SET_REGS, &regs);
   }
 
+  void copy_to_mem(unsigned long guest_addr, void *src, size_t size) {
+
+    // Search for memory slot containing guest_addr
+    for (const auto &memory : mem_vec) {
+      if (memory.guest_phys_addr + memory.memory_size <= guest_addr) {
+        continue;
+      }
+      dbg("copy_to_mem guest_addr {:#x}, found vm slot {} guest_addr {:#x} end "
+          "{:#x}\n",
+          guest_addr, memory.slot, memory.guest_phys_addr,
+          memory.guest_phys_addr + memory.memory_size);
+
+      // Get host memory pointer
+      unsigned char *guest_mem =
+          reinterpret_cast<unsigned char *>(memory.userspace_addr);
+
+
+      // If copy size bigger then current slot memory
+      // TODO: copy over slot boundaries
+      if(memory.memory_size < size){
+        panic("this slot only has {:#x} bytes of memory available", memory.memory_size);
+      }
+
+      // TODO: Remove after enough testing
+      if(memory.guest_phys_addr > guest_addr){
+        panic("BUG: This should not happen");
+      }
+
+      size_t offset = guest_addr - memory.guest_phys_addr;
+      memcpy(guest_mem + offset, src, size);
+      break;
+    }
+  }
+
   const struct vcpu &run() {
+    assert(!vcpus_vec.empty());
+
     const auto &vcpu = vcpus_vec.front();
     if (ioctl(vcpu.fd, KVM_RUN, 0) < 0) {
       panic("Could not execute KVM_RUN");
--- src/main.cpp
+++ src/main.cpp
@@ -1,94 +1,107 @@
 // Local include
 #include <KVM.h>
 
+#include <thread>
+
+#include <string.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
-#include <string.h>
 
 /* extern const unsigned char guest16[], guest16_end[]; */
 
-unsigned int guest_code[] = {
-0xf4
-};
+unsigned long guest_code[] = { 0xb82a00f4 };
 
-int main() {
-  // Create handle to kvm
-  KVM k = KVM();
-
-  // Get max number vcpus
-  cout << "Max vcpus: " << k.max_vcpus << endl;
-  cout << "Max shared vcpu mem: " << hex << showbase << k.max_vcpu_mmap_size << endl;
-
-  VM vm = VM(k, 0);
-
-  // Setup vm memory
-  vm.add_mem_pages(1);
-
-
-  // Why though?
-  vm.send(KVM_SET_TSS_ADDR, 0xfffbd000);
-
-  // Add vcpu
-  vm.add_vcpu();
-
-  vm.set_real_mode();
-
-  unsigned char * guest_mem = (unsigned char*) vm.memory_vec.front().userspace_addr;
-  // Copy code
-  guest_mem[0] = 0xf4;
-  /* memcpy(guest_mem, guest_code, sizeof(guest_code)); */
-
-  for(int i =0; i < 10; i++){
-    fmt::print("{:2x}", guest_mem[i]);
-  }
-  cout << endl;
-
-  while(true){
+void vm_thread(VM& vm){
+  while (true) {
     cout << "Starting vm " << endl;
-    const struct vcpu& vcpu0 = vm.run();
+    const struct vcpu &vcpu0 = vm.run();
     cout << "Exited vm " << endl;
 
     unsigned int exit_reason = vcpu0.kvm_run->exit_reason;
-    switch(exit_reason){
-      case KVM_EXIT_HLT:
-        goto check;
+    switch (exit_reason) {
+    case KVM_EXIT_HLT:
+      goto check;
 
-      // Print output written to port 0xE9
-      case KVM_EXIT_IO:
-        if(vcpu0.kvm_run->io.direction == KVM_EXIT_IO_OUT &&
-            vcpu0.kvm_run->io.port == 0xE9){
-          char *p = (char*) vcpu0.kvm_run;
-          fwrite(p + vcpu0.kvm_run->io.data_offset, vcpu0.kvm_run->io.size, 1, stdout);
-          fflush(stdout);
-          continue;
-        }
+    // Print output written to port 0xE9
+    case KVM_EXIT_IO:
+      if (vcpu0.kvm_run->io.direction == KVM_EXIT_IO_OUT &&
+          vcpu0.kvm_run->io.port == 0xE9) {
+        char *p = (char *)vcpu0.kvm_run;
+        fwrite(p + vcpu0.kvm_run->io.data_offset, vcpu0.kvm_run->io.size, 1,
+               stdout);
+        fflush(stdout);
+        continue;
+      }
 
-      default:
-        panic("Exit reason not handled: {}", exit_reason);
+    default:
+      panic("Exit reason not handled: {}", exit_reason);
     }
   }
 
 check:
   struct kvm_regs regs;
   unsigned long memval;
-	if (vm.send_vcpu(KVM_GET_REGS, &regs) < 0) {
-		perror("KVM_GET_REGS");
-		exit(1);
-	}
+  if (vm.send_vcpu(KVM_GET_REGS, &regs) < 0) {
+    perror("KVM_GET_REGS");
+    exit(1);
+  }
 
-	if (regs.rax != 42) {
-		printf("Wrong result: {E,R,}AX is %lld\n", regs.rax);
-		return 0;
-	}
+  if (regs.rax != 42) {
+    printf("Wrong result: {E,R,}AX is %lld\n", regs.rax);
+    exit(1);
+  }
 
-  char* mem = (char*) vm.memory_vec.at(0).userspace_addr;
-	memcpy(&memval, mem + 0x400, 8);
-	if (memval != 42) {
-		printf("Wrong result: memory at 0x400 is %lld\n",
-		       (unsigned long long)memval);
-		return 0;
-	}
+  cout << "Successfully executed vm with ax == 42" << endl;
+  exit(0);
+}
 
-	return 1;
+int main() {
+  // Create handle to kvm
+  KVM k = KVM();
 
+  dbg("Max mem slots {}\n", k.max_nr_mem_slots);
+
+  // Create vm
+  VM vm = VM(k, 0);
+
+  // Give vm one page of memory
+  vm.add_mem_pages(2, 0);
+
+  // Add vcpu
+  vm.add_vcpu();
+
+  /* vm.add_mem_pages(3, 1); */
+  // Set vcpu into real mode
+  vm.set_real_mode();
+
+  // Copy hlt instruction to addr 0
+  vm.copy_to_mem(0, guest_code, sizeof(guest_code));
+
+  unsigned int* addr = (unsigned int*) (vm.mem_vec.front().userspace_addr);
+
+  for(int i =0; i < 4; i++){
+    fmt::print("{:4x}", addr[i]);
+  }
+  cout << endl;
+
+  std::thread first(vm_thread, std::ref(vm));
+
+  while(true){
+    struct kvm_regs regs;
+    unsigned long memval;
+    vm.send_vcpu(KVM_GET_REGS, &regs);
+
+    cout << "RIP: " << regs.rip << " RAX: " << regs.rax << endl;
+  }
+
+  first.join();
+ /* char *mem = (char *)vm.mem.value().userspace_addr; */
+  /* memcpy(&memval, mem + 0x400, 8); */
+  /* if (memval != 42) { */
+  /*   printf("Wrong result: memory at 0x400 is %lld\n", */
+  /*          (unsigned long long)memval); */
+  /*   return 0; */
+  /* } */
+
+  return 1;
 }
